de523445bf352d78f4ce0fe972e31b4ed2bbc7dc
Macarena Galluppi <galluppi.macarenasol@gmail.com>
2021-02-02 23:20:29 -0300

    agregue DOM y Storage a mi JS

--- js/script.js
+++ js/script.js
@@ -10,26 +10,31 @@ function Reserva (nombreCompleto, telefono, email, fechaEvento, horario, persona
 	}
 }
 
-var reservas = [];
-
+var reservas = JSON.parse(localStorage.getItem("Reservas"));
+if (reservas==null) {
+	reservas=[];
+}
 
 function presupuestar() {
-	var nombreCompleto = prompt("Ingrese su nombre completo");
-	var telefono = prompt("Ingrese un telefono de contacto");
-	var email = prompt("Ingrese su email");
-	var fechaEvento = prompt("Ingrese la fecha estimada del evento");
-	var horario = prompt("Ingrese horario estimado");
-	var personas = parseInt(prompt("Establezca numero de personas para el evento"));
+	var nombreCompleto = document.getElementById("nombreApellido").value;
+	var telefono = document.getElementById("telefono").value;
+	var email = document.getElementById("email").value;
+	var fecha = document.getElementById("fecha").value;
+	var horario = document.getElementById("horario").value;
+	var personas = document.getElementById("personas").value;
 
-	var nuevaReserva = new Reserva(nombreCompleto, telefono, email, fechaEvento, horario, personas);
+
+	var nuevaReserva = new Reserva(nombreCompleto, telefono, email, fecha, horario, personas);
 	
 	reservas.push(nuevaReserva);
-		console.log(reservas);
-		
-	console.log(nuevaReserva);
-		alert("El presupuesto es: " + nuevaReserva.calcularPresupuesto());
+	console.log(reservas);
 
-	
+	console.log("El presupuesto es: " + nuevaReserva.calcularPresupuesto());
+	localStorage.setItem("Reservas", JSON.stringify(reservas));
+
 }
+
+var reservasTitle = document.getElementsByClassName("reservasTitle");
+console.log(reservasTitle[0].innerHTML);
 
 
