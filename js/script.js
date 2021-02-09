aa908a90e25bb43b3417216b53db4fe88d9cfecd
Macarena Galluppi <galluppi.macarenasol@gmail.com>
2021-02-09 00:44:55 -0300

    agregue jquery al proyecto en la seccion Reservas, validar los campos (completos o incompletos), agregue mostrar un alert cuando esta seleccionado el radioButton OTROS cuando se hace un presupuesto. Agregue el alert en el html en la seccion Reservas.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-function Reserva (nombreCompleto, telefono, email, fechaEvento, horario, personas) {
+function Reserva (nombreCompleto, telefono, email, fechaEvento, horario, personas, almuerzo, cena, evento) {
 	this.nombreCompleto = nombreCompleto;
 	this.telefono = telefono;
 	this.email = email;
 	this.fechaEvento = fechaEvento;
 	this.horario = horario;
 	this.personas = personas;
+	this.almuerzo = almuerzo;
+	this.cena = cena;
+	this.evento = evento;
 	this.calcularPresupuesto = function() {
 		return this.personas * 1500;
 	}
@@ -15,6 +18,22 @@ if (reservas==null) {
 	reservas=[];
 }
 
+
+function validarCampo() {
+	var valor = $(this).val();
+
+	if (valor == "" || valor == null) {
+		$(this).removeClass("is-valid");
+		$(this).addClass("is-invalid");
+	}
+	else {
+		$(this).removeClass("is-invalid");
+		$(this).addClass("is-valid");
+	}
+	
+}
+
+
 function presupuestar() {
 	var nombreCompleto = document.getElementById("nombreApellido").value;
 	var telefono = document.getElementById("telefono").value;
@@ -22,19 +41,30 @@ function presupuestar() {
 	var fecha = document.getElementById("fecha").value;
 	var horario = document.getElementById("horario").value;
 	var personas = document.getElementById("personas").value;
+	var almuerzo = document.getElementById("almuerzo").checked;
+	var cena = document.getElementById("cena").checked;
+	var evento = document.getElementById("evento").checked;
 
 
-	var nuevaReserva = new Reserva(nombreCompleto, telefono, email, fecha, horario, personas);
+	var nuevaReserva = new Reserva(nombreCompleto, telefono, email, fecha, horario, personas, almuerzo, cena, evento);
 	
 	reservas.push(nuevaReserva);
 	console.log(reservas);
 
-	console.log("El presupuesto es: " + nuevaReserva.calcularPresupuesto());
-	localStorage.setItem("Reservas", JSON.stringify(reservas));
+	if (evento == true) {
+		$("#alertPresupuesto").removeClass("d-none");
+		$("#resultadoPresupuesto").html (nuevaReserva.calcularPresupuesto());
+	}
+	else {
+		$("#alertPresupuesto").addClass("d-none");
+	}
 
+	localStorage.setItem("Reservas", JSON.stringify(reservas));
 }
 
-var reservasTitle = document.getElementsByClassName("reservasTitle");
-console.log(reservasTitle[0].innerHTML);
-
+$(document).ready(function() 
+{
+	document.getElementById("botonReserve").onclick=presupuestar;
+	$(".campo").blur(validarCampo);
+});
 
