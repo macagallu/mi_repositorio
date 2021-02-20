2de0f8b7ecf6447e21f1a23ba8c02b92c6a8f403
Macarena Galluppi <galluppi.macarenasol@gmail.com>
2021-02-20 17:25:00 -0300

    validar formulario mejorado y sugerir ingresar con mas precision los datos

--- js/script.js
+++ js/script.js
@@ -13,62 +13,34 @@ function Reserva (nombreCompleto, telefono, email, fechaEvento, horario, persona
 	}
 }
 
+var validador = null;
+
 var reservas = JSON.parse(localStorage.getItem("Reservas"));
 if (reservas==null) {
 	reservas=[];
 }
 
 
-function validarCampo(control) {
-	var valor = $(control).val();
-
-	if (valor == "" || valor == null) {
-		$(control).removeClass("is-valid");
-		$(control).addClass("is-invalid");
-		return false;
-	}
-	else {
-		$(control).removeClass("is-invalid");
-		$(control).addClass("is-valid");
-		return true;
-	}
-}
-
-function validarFormulario() {
-	var formularioValido = true;
-
-	$(".campo").each(function(){
-		var campoValido = validarCampo(this);
-
-		if (campoValido == false) {
-			formularioValido = false;
-		}
-	})
-
-	return formularioValido;
-
-}
-
 function presupuestar() {
-	if (validarFormulario() == false) {
-		$("#formularioIncompleto").removeClass("d-none");
+	if (validador.form() == false) {
+		$("#formularioIncompleto").fadeIn();
 		return;
 	}
 
 	else {
-		$("#formularioIncompleto").addClass("d-none");
+		$("#formularioIncompleto").fadeOut();
 	}
 
 
-	var nombreCompleto = document.getElementById("nombreApellido").value;
-	var telefono = document.getElementById("telefono").value;
-	var email = document.getElementById("email").value;
-	var fecha = document.getElementById("fecha").value;
-	var horario = document.getElementById("horario").value;
-	var personas = document.getElementById("personas").value;
-	var almuerzo = document.getElementById("almuerzo").checked;
-	var cena = document.getElementById("cena").checked;
-	var evento = document.getElementById("evento").checked;
+	var nombreCompleto = $("#nombreApellido").val();
+	var telefono = $("#telefono").val();
+	var email = $("#email").val();
+	var fecha = $("#fecha").val();
+	var horario = $("#horario").val();
+	var personas = $("#personas").val();
+	var almuerzo = $("#almuerzo").prop("checked");
+	var cena = $("#cena").prop("checked");
+	var evento = $("#evento").prop("checked");
 
 	var nuevaReserva = new Reserva(nombreCompleto, telefono, email, fecha, horario, personas, almuerzo, cena, evento);
 	
@@ -76,11 +48,11 @@ function presupuestar() {
 	console.log(reservas);
 
 	if (evento == true) {
-		$("#alertPresupuesto").removeClass("d-none");
+		$("#alertPresupuesto").fadeIn();
 		$("#resultadoPresupuesto").html (nuevaReserva.calcularPresupuesto());
 	}
 	else {
-		$("#alertPresupuesto").addClass("d-none");
+		$("#alertPresupuesto").fadeOut();
 	}
 
 	localStorage.setItem("Reservas", JSON.stringify(reservas));
@@ -92,5 +64,50 @@ $(document).ready(function() {
 		validarCampo(this);
 	});
 
-});
+	$("#exampleModal").modal();
 
+	validador = $("#formulario").validate({
+		errorClass: "is-invalid",
+		validClass: "is-valid",
+
+		rules: {
+			Nombre: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			telefono: {
+				required: true,
+				minlength: 8
+			},
+			date: "required",
+			personas: {
+				required: true,
+				minlength: 1,
+				min: 1
+			}
+		},
+		messages: {
+			Nombre: {
+				required: "Ingresa tu Nombre y Apellido.",
+				minlength: "Ingresa un nombre de 2 letras o más."
+			},
+			email: {
+				required: "Ingresa un email de contacto.",
+				email: "Ingresa un email valido. (ejemplo@tuemail.com)"
+			},
+			telefono: {
+				required: "Ingresa un telefono valido.",
+				minlength: "Ingresa un numero de telefono de 8 numeros o mas."
+			},
+			date: "Ingresa la fecha estimada del evento.",
+			personas: {
+				required: "Ingresa el numero de personas.",
+				min: "Ingresa un numero mayor a 1"
+			}
+		}
+	});
+});
